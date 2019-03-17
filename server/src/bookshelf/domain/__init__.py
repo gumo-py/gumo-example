21ceb6de015f74b5336d3746e9f6b6092c55fe86
Akira Yumiyama <yumiyama.akira@gmail.com>
2019-03-17 18:04:29 +0900

    Add key and KIND property to Book Object.

--- server/src/bookshelf/domain/__init__.py
+++ server/src/bookshelf/domain/__init__.py
@@ -2,6 +2,8 @@ import dataclasses
 
 from typing import List
 
+from gumo.datastore import EntityKey
+
 from bookshelf.domain.fundamental import BookTitle
 from bookshelf.domain.fundamental import BookAuthor
 from bookshelf.domain.fundamental import ISBN
@@ -9,7 +11,10 @@ from bookshelf.domain.fundamental import ISBN
 
 @dataclasses.dataclass(frozen=True)
 class Book:
+    key: EntityKey
     title: BookTitle
     primary_author: BookAuthor
     authors: List[BookAuthor]
     isbn: ISBN
+
+    KIND = 'Book'
