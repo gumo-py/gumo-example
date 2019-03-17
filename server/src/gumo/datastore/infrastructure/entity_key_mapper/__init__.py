a754efcea09b4bd6cbf53a3fe03e64469f6aa5a1
Akira Yumiyama <yumiyama.akira@gmail.com>
2019-03-17 09:39:33 +0900

    Implements EntityKeyMapper

--- server/src/gumo/datastore/infrastructure/entity_key_mapper/__init__.py
+++ server/src/gumo/datastore/infrastructure/entity_key_mapper/__init__.py
@@ -1,12 +1,19 @@
-from gumo.datastore.domain.entity_key import EntityKey
 from google.cloud import datastore
+
+from gumo.datastore.domain.entity_key import EntityKey
+from gumo.datastore.domain.entity_key import EntityKeyFactory
+
+from gumo.core import configure_once
 
 
 class EntityKeyMapper:
-    @classmethod
-    def to_entity_key(cls, datastore_key: datastore.Key) -> EntityKey:
-        raise NotImplementedError()
+    def to_entity_key(self, datastore_key: datastore.Key) -> EntityKey:
+        entity_key = EntityKeyFactory().build_from_pairs(pairs=datastore_key.path)
+        return entity_key
 
-    @classmethod
-    def to_datastore_key(cls, entity_key: EntityKey) -> datastore.Key:
-        raise NotImplementedError()
+    def to_datastore_key(self, entity_key: EntityKey) -> datastore.Key:
+        # TODO: configuration は DI で注入するようにしたい
+        project = configure_once().google_cloud_project.project_id
+        datastore_key = datastore.Key(*entity_key.flat_pairs(), project=project)
+
+        return datastore_key
---
+++ server/test/gumo/datastore/infrastructure/entity_key_mapper/entity_key_mapper_test.py
@@ -0,0 +1,34 @@
+import mytest
+
+from google.cloud import datastore
+
+from gumo.datastore.domain.entity_key import EntityKeyFactory
+from gumo.datastore.domain.entity_key import EntityKey
+
+from gumo.datastore.infrastructure.entity_key_mapper import EntityKeyMapper
+
+
+def test_to_datastore_key():
+    entity_key = EntityKeyFactory().build_from_pairs(pairs=[
+        ('Book', 'name'),
+        ('BookComment', 'comment'),
+    ])
+    datastore_key = EntityKeyMapper().to_datastore_key(entity_key=entity_key)
+
+    assert isinstance(datastore_key, datastore.Key), 'must be a datastore.Key'
+    assert datastore_key.flat_path  == ('Book', 'name', 'BookComment', 'comment')
+    assert datastore_key.kind == 'BookComment'
+    assert datastore_key.name == 'comment'
+
+
+def test_to_entity_key():
+    datastore_key = datastore.Key(
+        'Book', 'name', 'BookComment', 'comment',
+        project='test-project'
+    )
+    entity_key = EntityKeyMapper().to_entity_key(datastore_key=datastore_key)
+
+    assert isinstance(entity_key, EntityKey)
+    assert entity_key.flat_pairs() == ['Book', 'name', 'BookComment', 'comment']
+    assert entity_key.kind() == 'BookComment'
+    assert entity_key.name() == 'comment'
