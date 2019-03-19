eae7b978314891356a58608fb85b2b02c299ead6
Akira Yumiyama <yumiyama.akira@gmail.com>
2019-03-19 13:38:50 +0900

    Add Dependency Injection settings of GumoConfiguration, DatastoreConfiguration

---
+++ server/src/gumo/datastore/bind.py
@@ -0,0 +1,6 @@
+from gumo.datastore import DatastoreConfiguration
+from gumo.datastore import get_datastore_config
+
+
+def bind(binder):
+    binder.bind(DatastoreConfiguration, to=get_datastore_config())
