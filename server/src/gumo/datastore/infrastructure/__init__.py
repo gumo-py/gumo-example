e664cc2cbb947775059578ee11cb14d6009ff446
Akira Yumiyama <yumiyama.akira@gmail.com>
2019-03-20 19:04:41 +0900

    Expose EntityKeyMapper

--- server/src/gumo/datastore/infrastructure/__init__.py
+++ server/src/gumo/datastore/infrastructure/__init__.py
@@ -1,6 +1,8 @@
 from gumo.datastore.infrastructure.repository import DatastoreRepositoryMixin
+from gumo.datastore.infrastructure.entity_key_mapper import EntityKeyMapper
 
 
 __all__ = [
     DatastoreRepositoryMixin.__name__,
+    EntityKeyMapper.__name__,
 ]
