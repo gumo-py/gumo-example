7580360b5d0c54e8c5a560d03f678c1419b4d16c
Akira Yumiyama <yumiyama.akira@gmail.com>
2019-03-27 22:25:55 +0900

    remove gumo

--- server/src/gumo/task/bind.py
+++
@@ -1,6 +0,0 @@
-from gumo.task.application.repository import GumoTaskRepository
-from gumo.task.infrastructure.repository import GumoTaskRepositoryImpl
-
-
-def task_bind(binder):
-    binder.bind(GumoTaskRepository, to=GumoTaskRepositoryImpl)
